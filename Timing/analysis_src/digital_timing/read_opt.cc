4463137b2b50ad0e136e58ca41a056d25b13c872
andrea <andry3vi@gmail.com>
2017-11-29 19:21:46 +0100

    read_opt.cc debug version

--- Timing/analysis_src/digital_timing/read_opt.cc
+++ Timing/analysis_src/digital_timing/read_opt.cc
@@ -38,12 +38,60 @@ void read_opt(string filename)
 
   branch->SetAddress(&opt_sett);
 
+	std::vector<TCanvas * > Canvas;
+	//TCanvas * c = 0;
 
+	for (size_t i = 0; i < n_ev; i++)
+  {
+    branch->GetEntry(i);
+
+		//get parameters
+		float Fraction = opt_sett->factor;
+		int   Delay    = opt_sett->delay;
+		int   ZCL      = opt_sett->zcl;
+
+		TCanvas * c = new TCanvas(Form("Canvas %i", i),Form("Frac%.2f_Del%i_Zcl%i", Fraction, Delay, ZCL), 1280,800);
+
+		Canvas.push_back(c);
+	}
+
+	TH1F * Time = 0;
+	TH2F * TimeVsAmp = 0;
 
   for (size_t i = 0; i < n_ev; i++)
   {
     branch->GetEntry(i);
 
+		//get parameters
+		float Fraction = opt_sett->factor;
+		int   Delay    = opt_sett->delay;
+		int   ZCL      = opt_sett->zcl;
+
+		Time      = new TH1F("Time"       , Form("Frac%.2f_Del%i_Zcl%i_Timing", Fraction, Delay, ZCL), 1000, -10, 10);
+		TimeVsAmp = new TH2F("Time Vs Amp", Form("Frac%.2f_Del%i_Zcl%i_TimVsAmp", Fraction, Delay, ZCL), 1000, 0, 500, 1000, -10, 10);
+
+		Time->GetXaxis()->SetTitle("Timing [ns]");
+  	Time->GetYaxis()->SetTitle("Count");
+
+		TimeVsAmp->GetXaxis()->SetTitle("Energy [keV]");
+  	TimeVsAmp->GetYaxis()->SetTitle("Timing [ns]");
+
+
+    for (size_t j = 0; j < opt_sett->TimeA.size() ; j++)
+		{
+			Time->Fill(opt_sett->TimeA[j]-opt_sett->TimeB[j]);
+			TimeVsAmp->Fill(opt_sett->Energy[j],opt_sett->TimeA[j]-opt_sett->TimeB[j]);
+		}
+
+		Canvas[i]->Divide(2,1);
+
+		Canvas[i]->cd(1);
+		Time->Draw();
+
+		Canvas[i]->cd(2);
+		TimeVsAmp->Draw("COLZ");
+
+		Canvas[i]->Update();
 
   }
 
