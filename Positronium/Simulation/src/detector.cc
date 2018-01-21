4cb490ee07b6766950ca3bd5ebcba447e0480a26
Luca Morselli <luca.morselli8@gmail.com>
2018-01-21 12:45:35 +0100

    Generalized e_orthogonal finding

--- Positronium/Simulation/src/detector.cc
+++ Positronium/Simulation/src/detector.cc
@@ -1,38 +1,54 @@
 #include <detector.h>
 #include <armadillo>
 #include <fstream>
+#include <chrono>
+#include <ctime>
+#include <random>
 
-Detector::Detector(){}
-Detector::Detector(arma::vec x, double r)
+CylindricalDetector::CylindricalDetector(){}
+CylindricalDetector::CylindricalDetector(arma::vec x, double r)
 {
+  // Setting position & radius
   position = x;
   radius = r;
 
-  arma::vec e2 = {0.0,1.0,0.0};
+  // Finding a generic orthogonal vector
+  arma::vec e_orthogonal;
 
-  arma::vec e_r = normalise(cross(position,e2));
+  // Random generator
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  std::mt19937 generator (seed);
+  std::uniform_real_distribution<double> uniform01(0.0, 1.0);
+
+  // useless control : the propability of generating a random unity vector
+  // equal to our detector position is negligible
+
+  do
+  {
+    e_orthogonal = {uniform01(generator), uniform01(generator), uniform01(generator)};
+    e_orthogonal = normalise(e_orthogonal);
+
+  } while( normalise(cross(position, e_orthogonal)) == 1);
+
+  arma::vec e_r = normalise(cross(position,e_orthogonal));
 
   e_r = radius*e_r;
 
   arma::vec d1_edges = e_r+position;
 
-  d1_edges.print("edges:");
+  //d1_edges.print("edges:"); debuggin purpouse
+
   half_aperture = acos((dot(d1_edges,position))/(norm(d1_edges)*norm(position)));
-
-  std::ofstream outfile("prova.txt");
-
-  outfile << position[0] << " " << position[1] << " " << position[2] << std::endl;
-  outfile << d1_edges[0] << " " << d1_edges[1] << " " << d1_edges[2] << std::endl;
 
 }
 
-double Detector::get_halfaperture(){ return half_aperture; }
+double CylindricalDetector::get_halfaperture(){ return half_aperture; }
 
-bool Detector::check_detection(arma::vec gamma_direction)
+bool CylindricalDetector::check_detection(arma::vec gamma_direction)
 {
-  double angle =  (dot(gamma_direction,position))/(norm(gamma_direction)*norm(position));
+  double cos_angle =  (dot(gamma_direction,position))/(norm(gamma_direction)*norm(position));
 
-  if ( angle > cos(half_aperture)) { return true; }
-  
+  if ( cos_angle > cos(half_aperture)) { return true; }
+
   return false;
 }
