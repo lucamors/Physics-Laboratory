7662a952e1f18752e27d28063bcb3070e2c8e438
lucamors <luca.morseli8@gmail.com>
2018-03-13 00:10:32 +0100

    Added macro for cleaning cross section data and realign channels

--- Compton/Analysis/CrossSection/clean_data.cc
+++ Compton/Analysis/CrossSection/clean_data.cc
@@ -1,7 +1,3 @@
-//
-// Cleaning data
-//
-
 /*
 *
 * Reading TTree from CAEN Digitizer
@@ -37,6 +33,8 @@ ClassImp(cleanEv)
 void clean_data(string input_file_name, string outfile_name)
 {
 
+
+
 	// Retrieving File
   TFile * input_file = new TFile(input_file_name.c_str());
 
@@ -48,27 +46,23 @@ void clean_data(string input_file_name, string outfile_name)
 
   // Linking
 
-  acqEventPSD_t event_ch_A, event_ch_B, event_ch_C;
+  acqEventPSD_t event_ch_A, event_ch_B;
 
-  TBranch * branch_ch_A, * branch_ch_B, *branch_ch_C;
+  TBranch * branch_ch_A, * branch_ch_B;
   branch_ch_A = daq_tree->GetBranch(Form("acq_ch%i",0));
   branch_ch_A->SetAddress(&event_ch_A.timetag);
-  branch_ch_B = daq_tree->GetBranch(Form("acq_ch%i",1));
+  branch_ch_B = daq_tree->GetBranch(Form("acq_ch%i",2));
   branch_ch_B->SetAddress(&event_ch_B.timetag);
-	branch_ch_C = daq_tree->GetBranch(Form("acq_ch%i",2));
-	branch_ch_C->SetAddress(&event_ch_C.timetag);
 
   // Align channels
 
   long int nEventsA = (long int) branch_ch_A->GetEntries();
   long int nEventsB = (long int) branch_ch_B->GetEntries();
-	long int nEventsC = (long int) branch_ch_C->GetEntries();
 
 	// Declaring Saving ttree structure
 
 	cleanEv * event_ch_A_clean = new  cleanEv;
 	cleanEv * event_ch_B_clean = new  cleanEv;
-	cleanEv * event_ch_C_clean = new  cleanEv;
 
 	TFile * outfile = new TFile(outfile_name.c_str(),"RECREATE");
 
@@ -76,7 +70,7 @@ void clean_data(string input_file_name, string outfile_name)
 
 	out_tree->Branch("acq_ch0",&event_ch_A_clean);
 	out_tree->Branch("acq_ch1",&event_ch_B_clean);
-	out_tree->Branch("acq_ch2",&event_ch_C_clean);
+
 
 	// aligning channels
 
@@ -84,23 +78,25 @@ void clean_data(string input_file_name, string outfile_name)
 	int skipA = 0;
 	int counter_B = 0;
 	int skipB = 0;
-	int counter_C = 0;
-	int skipC = 0;
 	int coincident_events = 0;
 
 	int id_A = 0;
 	int id_B = 0;
-	int id_C = 0;
 
 	long int time_difference = 0;
 
+  // Declaring timetag statistics histograms
+	// Debugging Purpouse
+  // TH1F * timetag_histo_ch02 = new TH1F("timetag_h_ch02", "timetag histo ch02",100, -10, 10);
 
-	while(counter_A < nEventsA and counter_B < nEventsB and counter_C < nEventsC)
+
+
+	while(counter_A < nEventsA and counter_B < nEventsB)
 	{
 
 	    // Display Progress
 
-	    if (counter_A % (nEventsA/100) == 0 or counter_B % (nEventsB/100) == 0 or counter_C % (nEventsC/100) == 0)
+	    if (counter_A % (nEventsA/100) == 0 or counter_B % (nEventsB/100) == 0)
 	    {
 	        system("clear");
 	        std::cout << "Loading Event " << counter_A << endl;
@@ -113,38 +109,21 @@ void clean_data(string input_file_name, string outfile_name)
 	                  <<  (int)((counter_B*100.0)/nEventsB)
 	                  << "% in channel B"
 	                  << std::endl;
-	        std::cout << "Load "
-	                  <<  (int)((counter_C*100.0)/nEventsC)
-	                  << "% in channel C"
-	                  << std::endl;
 	    }
 
 	    // Events filling
 
 	    branch_ch_A->GetEntry(counter_A);
 	    branch_ch_B->GetEntry(counter_B);
-	    branch_ch_C->GetEntry(counter_C);
-      std::map<ULong64_t, int> Times;
-		  Times[event_ch_A.timetag]=0;
-		  Times[event_ch_B.timetag]=1;
-		  Times[event_ch_C.timetag]=2;
 
-			std::vector<ULong64_t> Timetag = {event_ch_A.timetag,event_ch_B.timetag,event_ch_C.timetag};
-			std::sort(Timetag.begin(),Timetag.end());
+	    time_difference = event_ch_A.timetag-event_ch_B.timetag;
 
-			ULong64_t TimeDiff=Timetag[2]-Timetag[0];
+			// Debugging Purpouse
+			// timetag_histo_ch02->Fill(time_difference);
 
-
-	    if(TimeDiff > 12.5) //  <--- scegli la finestra di allinamento in sample 1sample=4ns
+	    if(TMath::Abs(time_difference) > 5)
 	    {
-				  switch(Times[Timetag[0]]){
-						case 0:
-						counter_A++; skipA++; break;
-						case 1:
-						counter_B++; skipB++; break;
-						case 2:
-						counter_C++; skipC++; break;
-					}
+	        if( time_difference > 0){ counter_B++; skipB++; } else { counter_A++; skipA++; }
 	    }
 	    else
 	    {
@@ -155,8 +134,6 @@ void clean_data(string input_file_name, string outfile_name)
 				event_ch_B_clean->timetag = event_ch_B.timetag;
 				event_ch_B_clean->qlong = event_ch_B.qlong;
 
-				event_ch_C_clean->timetag = event_ch_C.timetag;
-				event_ch_C_clean->qlong = event_ch_C.qlong;
 
 
 				out_tree->Fill();
@@ -166,16 +143,18 @@ void clean_data(string input_file_name, string outfile_name)
 				coincident_events++;
 	      id_A++;
 	      id_B++;
-				id_C++;
 	      counter_A++;
 	      counter_B++;
-        counter_C++;
+
     }
 	}
 
-  cout<<"coincidences "<<coincident_events<<endl<<"tot events "<<nEventsA<<endl;
+	// Debugging Purpouse
+	// timetag_histo_ch02->Write();
+
 	out_tree->Write();
 	outfile->Close();
+
 
   return ;
 }
