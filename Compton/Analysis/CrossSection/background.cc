4af8943e64341c97a7495e71b07d10a390c0bf59
lucamors <luca.morseli8@gmail.com>
2018-03-13 10:48:31 +0100

    bg sub macro

---
+++ Compton/Analysis/CrossSection/background.cc
@@ -0,0 +1,23 @@
+
+void back_sub(string filename_noscat, string filename_scat)
+{
+	TFile * file_noscat = new TFile(filename_noscat.c_str());
+	TFile * file_scat = new TFile(filename_scat.c_str());
+
+	TH1F * noscat_h = (TH1F *) file_noscat->Get("ch2");
+	TH1F * scat_h = (TH1F *) file_scat->Get("ch2");
+
+	double current_bg, current_wbg;
+
+	for (size_t i = 0; i < 3500; i++)
+	{
+		current_bg = 2 * noscat_h->GetBinContent(i);
+		current_wbg = scat_h->GetBinContent(i);
+
+		scat_h->SetBinContent(i, current_wbg - current_bg );
+	}
+
+	scat_h->Draw();
+
+	return ;
+}
