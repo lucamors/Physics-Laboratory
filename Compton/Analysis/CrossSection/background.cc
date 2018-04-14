0b3dbe29e21df8c4eb39e109b0156b510d287953
andry3vi <andrea.raggio@studenti.unipd.it>
2018-04-14 15:33:25 +0200

    bs

--- Compton/Analysis/CrossSection/background.cc
+++ Compton/Analysis/CrossSection/background.cc
@@ -17,8 +17,19 @@ void back_sub(string filename_noscat, string filename_scat)
 		scat_h->SetBinContent(i, current_wbg - current_bg );
 	}
 
-	scat_h->Draw();
-	double gamma=  scat_h->Integral(scat_h->GetXaxis()->FindBin(215.5),scat_h->GetXaxis()->FindBin(295.5));
-  cout<<gamma<<endl;
+
+  scat_h->GetXaxis()->SetRangeUser(0,1450);
+  scat_h->GetYaxis()->SetRangeUser(0,165);
+  scat_h->GetXaxis()->SetTitle("Energy [keV]");
+  scat_h->SetStats(kFALSE);
+  scat_h->SetTitle("");
+  scat_h->Draw();
+
+  TH1F *h1c = (TH1F*)scat_h->Clone();
+  h1c->SetFillColorAlpha(kRed,0.80);
+  h1c->SetFillStyle(3003);
+	h1c->GetXaxis()->SetRangeUser(215.5,295.5);
+  h1c->Draw("SAME");
+
 	return ;
 }
